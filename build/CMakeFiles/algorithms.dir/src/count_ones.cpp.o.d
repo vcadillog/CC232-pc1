fc0e4ae5be092ea412044c47f71b6273a19879cf
Victor Cadillo <vcadillog@uni.pe>
2026-04-10 10:01:21 -0500

    eliminado build

--- build/CMakeFiles/algorithms.dir/src/count_ones.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/algorithms.dir/src/count_ones.cpp.o: \
- /home/victor/clases/algoritmos/CC232-pc1/src/count_ones.cpp \
- /usr/include/stdc-predef.h \
- /home/victor/clases/algoritmos/CC232-pc1/include/count_ones.h \
- /home/victor/clases/algoritmos/CC232-pc1/include/loop_counter.h
--- build/CMakeFiles/algorithms.dir/src/loop_counter.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/algorithms.dir/src/loop_counter.cpp.o: \
- /home/victor/clases/algoritmos/CC232-pc1/src/loop_counter.cpp \
- /usr/include/stdc-predef.h \
- /home/victor/clases/algoritmos/CC232-pc1/include/loop_counter.h
